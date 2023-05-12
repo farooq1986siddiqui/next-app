d4e6abbce826f00e03a81ba32c33ad3264db423e
farooq1986siddiqui <125255480+farooq1986siddiqui@users.noreply.github.com>
2023-05-12 23:25:40 +0500

    Update page.tsx

--- src/app/Product-list/page.tsx
+++ src/app/Product-list/page.tsx
@@ -1,10 +1,16 @@
-// import Link from "next/link"
+import Link from "next/link"
 import Nav from "../../../components/navbar"
 export default function Products () {
     return(
         <div>
         <Nav/> 
       <h1>LIST OF ALL PRODUCTS HERE</h1>
+      <ol>
+        <li><Link href="Product-list/product">Camera</Link></li>
+        <li><Link href="Product-list/product">Mobile</Link></li>
+        <li><Link href="Product-list/product">Purse</Link></li>
+        <li><Link href="Product-list/product">Laptop</Link></li>
+      </ol>
         </div>
     )
 }
